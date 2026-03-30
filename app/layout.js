c28b8bdbbfe8cf3dcc512a0bf8ef9cc924402c57
uncledave2082 <uncledave2082@gmail.com>
2026-03-30 00:15:49 -0400

    Simplify RootLayout component syntax

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,7 @@ export const metadata = {
   description: "Fat loss tracker",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
