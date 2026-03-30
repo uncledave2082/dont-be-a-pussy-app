6e12b23b133973b8684e6e6ace08c4e40c1b6828
uncledave2082 <uncledave2082@gmail.com>
2026-03-29 23:43:54 -0400

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Don't be a pussy",
+  description: "Fat loss tracker",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
